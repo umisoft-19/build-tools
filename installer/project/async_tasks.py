fda96b2f3bd6aa929f5071dc4fc0c73a30a6722b
Caleb Kandoro <kandoroc@hotmail.com>
2020-02-10 14:40:08 +0200

    tweaks

--- installer/project/async_tasks.py
+++ installer/project/async_tasks.py
@@ -218,7 +218,8 @@ def install():
 
     res = subprocess.run(['service.exe', 'start'], env=ENVIRONMENT)
     if res.returncode != 0:
-        write message("failed to start service, please restart your machine to finish installation.")
+        write_message("Failed to start service. Please restart your machine to finish  the installation.")
+    
 
     write_message("Installed Application successfully.")
     set_progress(100, success=True)
